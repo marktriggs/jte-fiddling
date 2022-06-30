b9e294dfbc2c4068935d2cc76c15daa63939a0ec
Mark Triggs <mark@dishevelled.net>
2022-06-30 13:58:12 +1000

    More noodling

--- src/main/java/net/dishevelled/App.java
+++ src/main/java/net/dishevelled/App.java
@@ -1,6 +1,8 @@
 package net.dishevelled;
 
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 
 import gg.jte.CodeResolver;
@@ -8,18 +10,31 @@ import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
 import gg.jte.TemplateOutput;
 import gg.jte.output.PrintWriterOutput;
-import gg.jte.output.StringOutput;
-import gg.jte.html.HtmlTemplateOutput;
 import gg.jte.resolve.ResourceCodeResolver;
 
 public class App
 {
+    public record FileEntry(String filename, int size, String mimeType) {}
+
     public static void main( String[] args )
     {
         CodeResolver codeResolver = new ResourceCodeResolver("templates");
         TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
 
-        TemplateOutput output = new PrintWriterOutput(new PrintWriter(System.out));
-        templateEngine.render("index.jte", new HashMap<String, Object>(), output);
+        try (PrintWriter stdout = new PrintWriter(System.out)) {
+            TemplateOutput output = new PrintWriterOutput(stdout);
+
+            HashMap<String, Object> params = new HashMap<String, Object>();
+
+            params.put("name", "Mark");
+            params.put("now", new Date());
+            params.put("files", Arrays.asList(
+                                              new FileEntry("/etc/passwd", 3100, "text/plain"),
+                                              new FileEntry("/etc/shadow", 6100, "text/plain"),
+                                              new FileEntry("/dev/zero", 0, "application/octet-stream")
+                                              ));
+
+            templateEngine.render("index.jte", params, output);
+        }
     }
 }
